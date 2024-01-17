e2128d1b44dce9192332289a5c0917270d787600
costanzapistone <c.p.pistone@tudelft.nl>
2024-01-17 18:12:28 +0100

    clean

--- code/Teleop_Keyboard/teleop_keyboard.py
+++ code/Teleop_Keyboard/teleop_keyboard.py
@@ -20,7 +20,7 @@ class TeleopKeyboard(Panda):
     # Define constants
     SUBJECT = 'b'
     MAT_FILE = f'/home/costanza/Robot-Control-by-EEG-with-ML/data/BCICIV_calib_ds1{SUBJECT}.mat'
-    MODEL_PATH = '/home/costanza/Robot-Control-by-EEG-with-ML/trained_model/trained_model_best.joblib'
+    TRAINED_ML_MODEL_PATH = '/home/costanza/Robot-Control-by-EEG-with-ML/trained_model/trained_model_best.joblib'
     
 
     def __init__(self):
@@ -37,6 +37,9 @@ class TeleopKeyboard(Panda):
         # Variable to store the MI class
         self.key = None
 
+        # List to store the key sequences
+        self.key_sequence = [] 
+        
         # Subscriber to keyboard from teleop_twist_keyboard package
         self.key_sub = rospy.Subscriber("/cmd_vel", Twist, self.keyboard_read_callback)
         
@@ -46,6 +49,105 @@ class TeleopKeyboard(Panda):
         # This funciton is needed to keep the node running 
         rospy.spin()
 
+    
+    def move_up(self):
+        """
+        Move the robot up by move_distance
+        """
+        # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
+        quat_goal = list_2_quaternion(self.curr_ori)
+        # Get current postion and add 1 mm to the z axis
+        goal = array_quat_2_pose(self.curr_pos + np.array([0.0, 0.0, self.move_distance]), quat_goal)
+        self.goal_pub.publish(goal)
+        self.key_value.linear.x = 0.0
+        self.key_value.angular.z = 0.0
+        print("moved Robot up 5 mm")
+
+    def move_down(self):
+        """
+        Move the robot down by move_distance
+        """
+        # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
+        quat_goal = list_2_quaternion(self.curr_ori)
+        # Get current postion and add 1 mm to the z axis
+        goal = array_quat_2_pose(self.curr_pos + np.array([0.0, 0.0, -self.move_distance]), quat_goal)
+        self.goal_pub.publish(goal)
+        self.key_value.linear.x = 0.0
+        self.key_value.angular.z = 0.0
+        print("moved Robot down 5 mm")
+
+    def move_left(self):
+        """
+        Move the robot left by move_distance
+        """
+        # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
+        quat_goal = list_2_quaternion(self.curr_ori)
+        # Get current postion and add 1 mm to the z axis
+        goal = array_quat_2_pose(self.curr_pos + np.array([0.0, self.move_distance, 0.0]), quat_goal)
+        self.goal_pub.publish(goal)
+        self.key_value.linear.x = 0.0
+        self.key_value.angular.z = 0.0
+        print("moved Robot left 5 mm")
+
+    def move_right(self):
+        """
+        Move the robot right by move_distance
+        """
+        # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
+        quat_goal = list_2_quaternion(self.curr_ori)
+        # Get current postion and add 1 mm to the z axis
+        goal = array_quat_2_pose(self.curr_pos + np.array([0.0, -self.move_distance, 0.0]), quat_goal)
+        self.goal_pub.publish(goal)
+        self.key_value.linear.x = 0.0
+        self.key_value.angular.z = 0.0
+        print("moved Robot right 5 mm")
+
+    def move_forward(self):
+        """
+        Move the robot forward by move_distance
+        """
+        # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
+        quat_goal = list_2_quaternion(self.curr_ori)
+        # Get current postion and add 1 mm to the z axis
+        goal = array_quat_2_pose(self.curr_pos + np.array([self.move_distance, 0.0, 0.0]), quat_goal)
+        self.goal_pub.publish(goal)
+        self.key_value.linear.x = 0.0
+        self.key_value.angular.z = 0.0
+        print("moved Robot forward 5 mm")
+
+    def move_backward(self):
+        """
+        Move the robot backward by move_distance
+        """
+        # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
+        quat_goal = list_2_quaternion(self.curr_ori)
+        # Get current postion and add 5 mm to the z axis
+        goal = array_quat_2_pose(self.curr_pos + np.array([-self.move_distance, 0.0, 0.0]), quat_goal)
+        self.goal_pub.publish(goal)
+        self.key_value.linear.x = 0.0
+        self.key_value.angular.z = 0.0
+        print("moved Robot backward 5 mm")
+    
+    def open_gripper(self):
+        """
+        Open the gripper by move_distance
+        """
+        gripper_width = self.gripper_width + self.move_distance
+        self.move_gripper(gripper_width)
+        self.key_value.linear.x = 0.0
+        self.key_value.angular.z = 0.0
+        print("Opened the gripper of 5 mm")
+
+    def close_gripper(self):
+        """
+        Close the gripper by move_distance
+        """
+        gripper_width = self.gripper_width - self.move_distance
+        self.move_gripper(gripper_width)
+        self.key_value.linear.x = 0.0
+        self.key_value.angular.z = 0.0
+        print("Closed the gripper of 5 mm")    
+        
     def keyboard_read_callback(self, key_input):
         """
         Callback function that changes the robots end effector cartesian pose when 
@@ -88,8 +190,6 @@ class TeleopKeyboard(Panda):
             # Get a random segment from the key 'right'
             self.key = list(fft_trials.keys())[1]
 
-        
-
         # Get a random segment based on the key
         segment = get_random_segment(fft_trials, self.key)
 
@@ -97,7 +197,7 @@ class TeleopKeyboard(Panda):
         X_test_lda = lda_one_segment(segment, self.SUBJECT)
 
         # Load the trained model
-        trained_model = joblib.load('/home/costanza/Robot-Control-by-EEG-with-ML/trained_model/trained_model_best.joblib')
+        trained_model = joblib.load(self.TRAINED_ML_MODEL_PATH)
 
         # Use the trained model to predict the class of the segment
         y_pred = trained_model.predict(X_test_lda)
@@ -107,200 +207,42 @@ class TeleopKeyboard(Panda):
 
         # If the predicted label is -1, move the robot to the left
         if y_pred == -1:
-            # key_input_pred = Twist()
             self.key_value.linear.x = 0.0
             self.key_value.angular.z = 1.0
         # If the predicted label is 1, move the robot to the right
         elif y_pred == 1:
-            # key_input_pred = Twist()
             self.key_value.linear.x = 0.0
             self.key_value.angular.z = -1.0
 
-            
+
         # Move along + Z axis
         if self.key_value.linear.x == 0.5 and self.key_value.angular.z == 0.0:
-            # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-            quat_goal = list_2_quaternion(self.curr_ori)
-            # Get current postion and add 1 mm to the z axis
-            goal = array_quat_2_pose(self.curr_pos + np.array([0.0, 0.0, self.move_distance]), quat_goal)
-            self.goal_pub.publish(goal)
-            self.key_value.linear.x = 0.0 
-            print("moved Robot up 5 mm")
-                
+            self.move_up()
 
         # - Z axis
         elif self.key_value.linear.x == -0.5 and self.key_value.angular.z == 0.0:
-            # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-            quat_goal = list_2_quaternion(self.curr_ori)
-            # Get current postion and add 1 mm to the z axis
-            goal = array_quat_2_pose(self.curr_pos + np.array([0.0, 0.0, -self.move_distance]), quat_goal)
-            self.goal_pub.publish(goal)
-            self.key_value.linear.x = 0.0 
-            print("moved Robot down 5 mm")
-
+            self.move_down()
 
         # + Y axis - j key - left
         elif self.key_value.linear.x == 0.0 and self.key_value.angular.z == 1.0:
-            # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-            quat_goal = list_2_quaternion(self.curr_ori)
-            # Get current postion and add 1 mm to the z axis
-            goal = array_quat_2_pose(self.curr_pos + np.array([0.0, self.move_distance, 0.0]), quat_goal)
-            self.goal_pub.publish(goal)
-            self.key_value.angular.z = 0.0
-            print("moved Robot left 5 mm")
-            
+            self.move_left()
+         
         # - Y axis - l key - right
         elif self.key_value.linear.x == 0.0 and self.key_value.angular.z == -1.0:
-            # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-            quat_goal = list_2_quaternion(self.curr_ori)
-            # Get current postion and add 1 mm to the z axis
-            goal = array_quat_2_pose(self.curr_pos + np.array([0.0, -self.move_distance, 0.0]), quat_goal)
-            self.goal_pub.publish(goal)
-            self.key_value.angular.z = 0.0
-            print("moved Robot right 5 mm")
-
+            self.move_right()
 
         # + X axis
         elif self.key_value.linear.x == 0.5 and self.key_value.angular.z == 1.0:
-            # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-            quat_goal = list_2_quaternion(self.curr_ori)
-            # Get current postion and add 1 mm to the z axis
-            goal = array_quat_2_pose(self.curr_pos + np.array([self.move_distance, 0.0, 0.0]), quat_goal)
-            self.goal_pub.publish(goal)
-            self.key_value.angular.x = 0.0
-            self.key_value.angular.z = 0.0
-            print("moved Robot left 5 mm")
-            
+            self.move_forward()
+       
         # - X axis
         elif self.key_value.linear.x == 0.0 and self.key_value.angular.z == -1.0:
-            # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-            quat_goal = list_2_quaternion(self.curr_ori)
-            # Get current postion and add 1 mm to the z axis
-            goal = array_quat_2_pose(self.curr_pos + np.array([-self.move_distance, 0.0, 0.0]), quat_goal)
-            self.goal_pub.publish(goal)
-            self.key_value.angular.x = 0.0
-            self.key_value.angular.z = 0.0
-            print("moved Robot right 5 mm")
+            self.move_backward()
 
         # Open the gripper (press m)
-        elif self.key_value.linear.x == -0.5 and self.key_value.angular.z == -1.0:     
-            gripper_width = self.gripper_width + self.move_distance
-            self.move_gripper(gripper_width)
-            self.key_value.angular.x = 0.0
-            self.key_value.angular.z = 0.0
-            print("Opened the gripper of 5 mm")
+        elif self.key_value.linear.x == -0.5 and self.key_value.angular.z == -1.0:
+            self.open_gripper()     
 
         # Close the gripper (press .)
-        elif self.key_value.linear.x == -0.5 and self.key_value.angular.z == 1.0:     
-            gripper_width = self.gripper_width - self.move_distance
-            self.move_gripper(gripper_width)
-            self.key_value.angular.x = 0.0
-            self.key_value.angular.z = 0.0
-            print("Closed the gripper of 5 mm")
-
-
-
-    # def keyboard_read_callback(self, key_input):
-    #     """
-    #     Callback function that changes the robots end effector cartesian pose when 
-    #     one of the arrow keys are pressed. It changes the equilibrium pose by 1mm, given the key pressed
-    #     and direction assigned to that key.
-        
-    #     Key - Action         
-    #     'i' = + z-axis
-    #     ',' = - z-axis
-    #     'j' = + y-axis
-    #     'l' = - y-axis
-    #     'u' = + x-axis
-    #     'o' = - x-axis
-    #     'm' = open the gripper
-    #     '.' = close the gripper
-    #     """
-    #     processed_key_input = self.process_key_input(key_input)
-    #     print(processed_key_input)
-    #     self.key_value = processed_key_input      
-
-    #     # self.key_value = key_input
-    #     print(self.key_value) 
-        
-    #     # Move along + Z axis
-    #     if self.key_value.linear.x == 0.5 and self.key_value.angular.z == 0.0:
-    #         # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-    #         quat_goal = list_2_quaternion(self.curr_ori)
-    #         # Get current postion and add 1 mm to the z axis
-    #         goal = array_quat_2_pose(self.curr_pos + np.array([0.0, 0.0, self.move_distance]), quat_goal)
-    #         self.goal_pub.publish(goal)
-    #         self.key_value.linear.x = 0.0 
-    #         print("moved Robot up 5 mm")
-            
-
-    #     # - Z axis
-    #     elif self.key_value.linear.x == -0.5 and self.key_value.angular.z == 0.0:
-    #         # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-    #         quat_goal = list_2_quaternion(self.curr_ori)
-    #         # Get current postion and add 1 mm to the z axis
-    #         goal = array_quat_2_pose(self.curr_pos + np.array([0.0, 0.0, -self.move_distance]), quat_goal)
-    #         self.goal_pub.publish(goal)
-    #         self.key_value.linear.x = 0.0 
-    #         print("moved Robot down 1 mm")
-
-
-    #     # + Y axis
-    #     elif self.key_value.linear.x == 0.0 and self.key_value.angular.z == 1.0:
-    #         # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-    #         quat_goal = list_2_quaternion(self.curr_ori)
-    #         # Get current postion and add 1 mm to the z axis
-    #         goal = array_quat_2_pose(self.curr_pos + np.array([0.0, self.move_distance, 0.0]), quat_goal)
-    #         self.goal_pub.publish(goal)
-    #         self.key_value.angular.z = 0.0
-    #         print("moved Robot left 1 mm")
-        
-    #     # - Y axis
-    #     elif self.key_value.linear.x == 0.0 and self.key_value.angular.z == -1.0:
-    #         # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-    #         quat_goal = list_2_quaternion(self.curr_ori)
-    #         # Get current postion and add 1 mm to the z axis
-    #         goal = array_quat_2_pose(self.curr_pos + np.array([0.0, -self.move_distance, 0.0]), quat_goal)
-    #         self.goal_pub.publish(goal)
-    #         self.key_value.angular.z = 0.0
-    #         print("moved Robot right 1 mm")
-
-
-    #     # + X axis
-    #     elif self.key_value.linear.x == 0.5 and self.key_value.angular.z == 1.0:
-    #         # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-    #         quat_goal = list_2_quaternion(self.curr_ori)
-    #         # Get current postion and add 1 mm to the z axis
-    #         goal = array_quat_2_pose(self.curr_pos + np.array([self.move_distance, 0.0, 0.0]), quat_goal)
-    #         self.goal_pub.publish(goal)
-    #         self.key_value.angular.x = 0.0
-    #         self.key_value.angular.z = 0.0
-    #         print("moved Robot left 1 mm")
-        
-    #     # - X axis
-    #     elif self.key_value.linear.x == 0.0 and self.key_value.angular.z == -1.0:
-    #         # Set this to the current value as it needs some orientation value to publish to /equilibrium_pose
-    #         quat_goal = list_2_quaternion(self.curr_ori)
-    #         # Get current postion and add 1 mm to the z axis
-    #         goal = array_quat_2_pose(self.curr_pos + np.array([-self.move_distance, 0.0, 0.0]), quat_goal)
-    #         self.goal_pub.publish(goal)
-    #         self.key_value.angular.x = 0.0
-    #         self.key_value.angular.z = 0.0
-    #         print("moved Robot right 1 mm")
-
-    #     # Open the gripper (press m)
-    #     elif self.key_value.linear.x == -0.5 and self.key_value.angular.z == -1.0:     
-    #         gripper_width = self.gripper_width + self.move_distance
-    #         self.move_gripper(gripper_width)
-    #         self.key_value.angular.x = 0.0
-    #         self.key_value.angular.z = 0.0
-    #         print("Opened the gripper of 5 mm")
-
-    #     # Close the gripper (press .)
-    #     elif self.key_value.linear.x == -0.5 and self.key_value.angular.z == 1.0:     
-    #         gripper_width = self.gripper_width - self.move_distance
-    #         self.move_gripper(gripper_width)
-    #         self.key_value.angular.x = 0.0
-    #         self.key_value.angular.z = 0.0
-    #         print("Closed the gripper of 5 mm")
-            
+        elif self.key_value.linear.x == -0.5 and self.key_value.angular.z == 1.0:
+            self.close_gripper()
